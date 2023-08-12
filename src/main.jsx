c7728f5041dd39e37c53a446fa5fcc37652d2077
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-08-12 12:25:01 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <BrowserRouter basename="/react-app"> не потрібно для firebase deploy*/}
-      <BrowserRouter>
+      <BrowserRouter basename="/payment-services">
         <App />
       </BrowserRouter>
     </Provider>
