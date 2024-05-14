d6caf7aabfd74393a0435e7f1f5dd8a0101eb20c
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-05-15 00:55:26 +0300

    fix async/await in stripe init file

--- src/helpers/initialStripe.js
+++ src/helpers/initialStripe.js
@@ -2,4 +2,6 @@ import { loadStripe } from "@stripe/stripe-js";
 
 const { VITE_STRIPE_PUBLIC_KEY } = import.meta.env;
 
-export const stripePromise = await loadStripe(VITE_STRIPE_PUBLIC_KEY);
+export const stripePromise = async () => {
+  return await loadStripe(VITE_STRIPE_PUBLIC_KEY);
+};
